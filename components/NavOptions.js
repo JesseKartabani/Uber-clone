6074e2656976cd65a1ad378b08678aeeae0f2ea8
Jesse1224 <jessekartabani@gmail.com>
2022-10-10 14:44:47 +1100

    Add NavOptions starting file

---
+++ components/NavOptions.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>NavOptions</Text>
+    </View>
+  );
+};
+
+export default NavOptions;
