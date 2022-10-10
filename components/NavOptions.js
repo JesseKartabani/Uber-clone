670705385ee4ae26e083ab70b11ef7710889fbbd
Jesse1224 <jessekartabani@gmail.com>
2022-10-10 15:24:26 +1100

    Add styling

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,7 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { FlatList } from "react-native-web";
+import tw from "twrnc";
 
 const data = [
   {
@@ -24,14 +25,14 @@ const NavOptions = () => {
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{ uri: item.image }}
             />
 
-            <Text>{item.title}</Text>
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
           </View>
         </TouchableOpacity>
       )}
