cdbcf299a85e409e156b4c98eea0aef88e136d18
Jesse1224 <jessekartabani@gmail.com>
2022-10-10 14:59:41 +1100

    Images for NavOptions

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,11 +1,39 @@
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { FlatList } from "react-native-web";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
