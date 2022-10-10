0bf2468b33462ae4a47e4af9baaac3a5207cd305
Jesse1224 <jessekartabani@gmail.com>
2022-10-10 19:27:28 +1100

    MapScreen file setup

---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>MapScreen</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
