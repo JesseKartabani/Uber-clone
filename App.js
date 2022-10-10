c6b6be319b94a6b87c827a58548e70f9e651f336
Jesse1224 <jessekartabani@gmail.com>
2022-10-10 16:20:46 +1100

    Add safe area provider

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
