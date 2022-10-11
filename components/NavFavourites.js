074d6db34ee58760e8f1d10811d1ebc8f2132638
Jesse1224 <jessekartabani@gmail.com>
2022-10-11 19:06:34 +1100

    NavFavourites icons

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,11 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { FlatList } from "react-native-gesture-handler";
+import { Icon } from "@rneui/base";
+import tw from "twrnc";
+
+const data = [
+  {
+    id: "123",
+    icon: "home",
+    location: "Home",
+    destination: "Code Street, London, UK",
+  },
+  {
+    id: "456",
+    icon: "briefcase",
+    location: "Work",
+    destination: "London Eye, London, UK",
+  },
+];
 
 const NavFavourites = () => {
   return (
-    <View>
-      <Text>NavFavourites</Text>
-    </View>
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item: { location, destination, icon } }) => (
+        <TouchableOpacity>
+          <Icon
+            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+            name={icon}
+            type="ionicon"
+            color="white"
+            size={18}
+          />
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
