30fadb6217fbf3b360feac5e607dd6d1b279dc1e
Jesse1224 <jessekartabani@gmail.com>
2022-10-11 17:37:37 +1100

    Navigate Card setup

---
+++ components/NavigateCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const NavigateCard = () => {
+  return (
+    <View>
+      <Text>NavigateCard</Text>
+    </View>
+  );
+};
+
+export default NavigateCard;
+
+const styles = StyleSheet.create({});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,16 +2,30 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import Map from "../components/Map";
+import NavigateCard from "../components/NavigateCard";
 import MapView from "react-native-maps";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
+
   return (
     <View>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
 
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   );
 };
