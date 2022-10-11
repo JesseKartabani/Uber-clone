004c1286b89204242943449714107d796769dcc9
Jesse1224 <jessekartabani@gmail.com>
2022-10-11 17:02:24 +1100

    Pin user location on map

--- components/Map.js
+++ components/Map.js
@@ -2,18 +2,32 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import MapView, { Marker } from "react-native-maps";
 import tw from "twrnc";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
 
 const Map = () => {
+  const origin = useSelector(selectOrigin);
+
   return (
     <MapView
       style={tw`flex-1`}
+      mapType="mutedStandard"
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
       }}
-    />
+    >
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+        />
+      )}
+    </MapView>
   );
 };
 
