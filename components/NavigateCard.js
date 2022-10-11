ab07ca3d8515affb212db315cb091a444016dbcb
Jesse1224 <jessekartabani@gmail.com>
2022-10-11 18:16:10 +1100

    Navigation for ride options

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -3,8 +3,14 @@ import React from "react";
 import tw from "twrnc";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning Jesse</Text>
@@ -17,7 +23,14 @@ const NavigateCard = () => {
             returnKeyType={"search"}
             minLength={2}
             onPress={(data, details = null) => {
-              //TODO
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+
+              navigation.navigate("RideOptionsCard");
             }}
             enablePoweredByContainer={false}
             query={{
