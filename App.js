69a0b9305a8c6012ed8f7d16866577c20cdf6d3c
Jesse1224 <jessekartabani@gmail.com>
2022-10-11 19:23:51 +1100

    Remove redundant style sheet

--- App.js
+++ App.js
@@ -35,12 +35,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
