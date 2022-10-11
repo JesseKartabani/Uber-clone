5eeb491ce1f0647f8474cecfff41716973c70424
Jesse1224 <jessekartabani@gmail.com>
2022-10-11 16:03:58 +1100

    Map screen setup

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import tw from "twrnc";
+import Map from "../components/Map";
+import MapView from "react-native-maps";
 
 const MapScreen = () => {
   return (
     <View>
-      <Text>MapScreen</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+
+      <View style={tw`h-1/2`}></View>
     </View>
   );
 };
