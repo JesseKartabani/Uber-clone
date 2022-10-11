25240ea5487d04dbdbbd708d79d64a58bb7c4c1b
Jesse1224 <jessekartabani@gmail.com>
2022-10-11 18:32:53 +1100

    Add map directions

--- components/Map.js
+++ components/Map.js
@@ -3,10 +3,13 @@ import React from "react";
 import MapView, { Marker } from "react-native-maps";
 import tw from "twrnc";
 import { useSelector } from "react-redux";
-import { selectOrigin } from "../slices/navSlice";
+import { selectDestination, selectOrigin } from "../slices/navSlice";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
 
   return (
     <MapView
@@ -19,6 +22,16 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      )}
+
       {origin?.location && (
         <Marker
           coordinate={{
