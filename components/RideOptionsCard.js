547d5a7585e83855c910b6924a7fb8e57e5dd031
Jesse1224 <jessekartabani@gmail.com>
2022-10-12 14:49:26 +1100

    Back button navigation for ride options

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
+import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
+import { FlatList } from "react-native-gesture-handler";
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
+    <View style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+
+      <FlatList />
     </View>
   );
 };
