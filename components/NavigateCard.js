d9a7d9288ffdf9e4857f55542123ad27c7d8ad7c
Jesse1224 <jessekartabani@gmail.com>
2022-10-12 14:07:26 +1100

    Rides button navigate to RideOptionsCard

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -52,6 +52,7 @@ const NavigateCard = () => {
         style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
       >
         <TouchableOpacity
+          onPress={() => navigation.navigate("RideOptionsCard")}
           style={tw`flex flex-row bg-black w-24 px-4 py-3 rounded-full justify-between`}
         >
           <Icon name="car" type="font-awesome" color="white" size={16} />
